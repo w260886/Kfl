497e566d8bb2fe112764a37cb2fbe5b4af2327ac
unknown <>
2017-03-01 16:08:53 +0800

    first updata

---
+++ kfl/data/init.php
@@ -0,0 +1,3 @@
+<?php
+$conn = mysqli_connect('127.0.0.1','root', '123456', 'kaifanla', 3306);
+mysqli_query($conn, "SET NAMES UTF8");
---
+++ kfl/data/order_add.php
@@ -0,0 +1,55 @@
+<?php
+
+header("Content-Type:application/json");
+
+@$user_name = $_REQUEST["user_name"];
+@$sex = $_REQUEST["sex"];
+@$phone = $_REQUEST["phone"];
+@$address = $_REQUEST["address"];
+@$did = $_REQUEST["did"];
+
+
+if(empty($user_name) || empty($sex) || empty($phone) || empty($address)
+|| empty($did))
+{
+    echo '[]';
+    return;
+}
+
+$order_time = time()*1000;
+
+
+require('init.php');
+
+$sql = "INSERT INTO kf_order VALUES(NULL,'$phone','$user_name','$sex','$order_time','$address','$did')";
+$result = mysqli_query($conn,$sql);
+
+$output = [];
+
+if($result)
+{
+    $arr['msg'] = 'succ';
+    $arr['oid'] = mysqli_insert_id($conn);
+}
+else
+{
+    $arr['msg'] = 'err';
+}
+
+$output[] = $arr;
+
+echo json_encode($output);
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+
